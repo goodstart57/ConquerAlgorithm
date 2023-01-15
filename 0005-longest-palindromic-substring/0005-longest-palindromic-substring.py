e0f9523a71394afd3346c37cc7637a2affd215d2
JAESEO LEE <goodstart57@gmail.com>
2023-01-15 22:58:50 +0900

    Time: 4533 ms (21.43%), Space: 21.9 MB (11.94%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,13 +1,37 @@
 class Solution:
-    def longestPalindrome(self, s: str) -> str:
-        length = len(s)
+#     # 풀이 1
+#     def longestPalindrome(self, s: str) -> str:
+#         length = len(s)
 
-        if length == 1:
-            return s
+#         if length == 1:
+#             return s
         
-        for length_substring in range(length, 0, -1):
-            for i_start in range(length - length_substring + 1):
-                if s[i_start:i_start+length_substring] == s[i_start:i_start+length_substring][::-1]:
-                    return s[i_start:i_start+length_substring]
+#         for length_substring in range(length, 0, -1):
+#             for i_start in range(length - length_substring + 1):
+#                 if s[i_start:i_start+length_substring] == s[i_start:i_start+length_substring][::-1]:
+#                     return s[i_start:i_start+length_substring]
         
-        return ""
+#         return ""
+    def longestPalindrome(self, s: str) -> str:
+        substring = ""
+        dp = [[0] * len(s) for _ in range(len(s))]
+        
+        for i in range(len(s)):
+            dp[i][i] = True
+            substring = s[i]
+        
+        for i in range(len(s)-1, -1, -1):
+            for j in range(i+1, len(s)):
+                if s[i] == s[j]:
+                    # print(f"({i}:{s[i]},{j}:{s[j]}) {s[i:j+1]}")
+                    if j - i == 1 or dp[i+1][j-1] == True:
+                        # print("  Accepted")
+                        dp[i][j] = True
+                        substring = max(substring, s[i:j+1], key=lambda x: len(x))
+        return substring
+        
+        
+        
+        
+        
+        

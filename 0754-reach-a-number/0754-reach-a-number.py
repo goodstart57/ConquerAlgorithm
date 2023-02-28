aa7833bd4c75285e7b5e92484549e753deafc49a
JAESEO LEE <goodstart57@gmail.com>
2023-02-28 20:33:54 +0900

    Time: 102 ms (35.20%), Space: 13.7 MB (93.60%) - LeetHub

--- 0754-reach-a-number/0754-reach-a-number.py
+++ 0754-reach-a-number/0754-reach-a-number.py
@@ -1,18 +1,11 @@
 class Solution:
     def reachNumber(self, target: int) -> int:
-        S, k, delta = 0, 0, 1
-        target = target if target > 0 else -target
-        
-        while True:
-            if S < target:
-                S, k = S+k+1, k+1
-            else:
-                delta = S - target
-                
-                if delta % 2 == 0:
-                    break
-                elif delta % 2 == 1:
-                    S, k = S+k+1, k+1
-                    
-        return k
-                
+        ans, k = 0, 0
+        target = abs(target)
+        while ans < target:
+            ans += k
+            k += 1
+        while (ans - target) % 2:
+            ans += k
+            k += 1
+        return k - 1  

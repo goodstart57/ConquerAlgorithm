c2dbfe66d3794e5a2452f9ce7940df5aa466c0da
JAESEO LEE <goodstart57@gmail.com>
2023-03-15 21:06:44 +0900

    Time: 26 ms (99.43%), Space: 14 MB (22.21%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -1,39 +1,17 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        if not (list1 or list2):
-            return None
-        elif not list1:
-            return list2
-        elif not list2:
-            return list1
-        elif list1.val > list2.val:
-            orgn_head = list2
-            list2 = list2.next
-        else:
-            orgn_head = list1
-            list1 = list1.next
-        
-        head = orgn_head
+        dummy = ListNode(0)
+        curr = dummy
         
         while list1 and list2:
-            if list1.val > list2.val:
-                head.next, list2 = list2, list2.next
+            if list1.val < list2.val:
+                curr.next = list1
+                list1 = list1.next
             else:
-                head.next, list1 = list1, list1.next
-            head = head.next
+                curr.next = list2
+                list2 = list2.next
+            curr = curr.next
         
-        while list1:
-            head.next, list1 = list1, list1.next
-            head = head.next
+        curr.next = list1 or list2
         
-        while list2:
-            head.next, list2 = list2, list2.next
-            head = head.next
-            
-        return orgn_head
-            
+        return dummy.next

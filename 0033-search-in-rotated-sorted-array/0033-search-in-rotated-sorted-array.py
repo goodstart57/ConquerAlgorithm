f990b87f4468293729dc3e157c905436a850635a
JAESEO LEE <goodstart57@gmail.com>
2023-04-04 19:55:02 +0900

    Time: 40 ms (78.57%), Space: 14.3 MB (11.29%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -10,17 +10,17 @@ class Solution:
             if nums[l] <= nums[mid]:
                 if nums[l] <= target <= nums[mid]:
                     # print("  # case1-1")
-                    r -= 1
+                    r = mid - 1
                 else:
                     # print("  # case1-2")
-                    l += 1
+                    l = mid + 1
             else:
                 if nums[mid] <= target <= nums[r]:
                     print("  # case2-1")
-                    l += 1
+                    l = mid + 1
                 else:
                     print("  # case2-2")
-                    r -= 1
+                    r = mid - 1
         
         return result
 

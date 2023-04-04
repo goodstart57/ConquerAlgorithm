23f92f627dd16da0c2316b6120a2e94388f0fae9
JAESEO LEE <goodstart57@gmail.com>
2023-04-04 19:53:55 +0900

    Time: 54 ms (7.02%), Space: 14.3 MB (11.29%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,7 +1,34 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        result = -1
-        if target in nums:
-            result = nums.index(target)
+        result, l, r = -1, 0, len(nums) -1 
+        while l <= r:
+            mid = (r + l) // 2
+            print(f"nums[{l}]:{nums[l]} / nums[{mid}]: {nums[mid]} / nums[{r}]: {nums[r]}")
+            if nums[mid] == target:
+                result = mid
+            
+            if nums[l] <= nums[mid]:
+                if nums[l] <= target <= nums[mid]:
+                    # print("  # case1-1")
+                    r -= 1
+                else:
+                    # print("  # case1-2")
+                    l += 1
+            else:
+                if nums[mid] <= target <= nums[r]:
+                    print("  # case2-1")
+                    l += 1
+                else:
+                    print("  # case2-2")
+                    r -= 1
         
         return result
+
+## CLEAR
+# class Solution:
+#     def search(self, nums: List[int], target: int) -> int:
+#         result = -1
+#         if target in nums:
+#             result = nums.index(target)
+        
+#         return result

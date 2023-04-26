dad3e2086c97e9334466f1e83e328c7cb5920d08
JAESEO LEE <goodstart57@gmail.com>
2023-04-26 21:29:57 +0900

    Time: 57 ms (13.52%), Space: 16.5 MB (6.00%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.py
+++ 0038-count-and-say/0038-count-and-say.py
@@ -1,25 +1,22 @@
 class Solution:
     def countAndSay(self, n: int) -> str:
-        if n==1: return '1'
-        
-        res = ['1']
-        for i in range(n-1):
-            i = 0
-            s = res[-1]
-            curr_s = ''
-            j = 0
-            while i<len(s):
-                c = 0
-                while j<len(s) and s[i] == s[j]:
-                    c+=1
-                    j+=1
-                curr_s += str(c)
-                curr_s += str(s[i])
-                i = j
-                
-            res.append(curr_s)
-        
-        return res[-1]
+        dp = [ "" for i in range(0,n+1)]
+        dp[1] = "1 "
+        i = 2 ;
+        while( i < n+1 ):
+            print(f'i:{i} dp[{i-1}]:{dp[i-1]}')
+            c = 0 
+            for j in range(0,len(dp[i-1])-1):
+                if( dp[i-1][j] == dp[i-1][j+1]):
+                    c += 1;
+                else:
+                    dp[i] += chr(c+1+ord('0')) + dp[i-1][j] ;
+                    c=0;
+            dp[i] += ' ';
+            i +=1
+            
+
+        return dp[-1][:-1];
 """class Solution:
     def count(self, s):
         si, ei, result = 0, 0, ""

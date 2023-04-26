2e1b9fa4127e44e4f4fc8aece1400ce93c1f093d
JAESEO LEE <goodstart57@gmail.com>
2023-04-26 21:31:17 +0900

    Time: 40 ms (87.81%), Space: 14.2 MB (11.66%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.py
+++ 0038-count-and-say/0038-count-and-say.py
@@ -1,44 +1,25 @@
 class Solution:
-    def countAndSay(self, n: int) -> str:
-        dp = [ "" for i in range(0,n+1)]
-        dp[1] = "1 "
-        i = 2 ;
-        while( i < n+1 ):
-            print(f'i:{i} dp[{i-1}]:{dp[i-1]}')
-            c = 0 
-            for j in range(0,len(dp[i-1])-1):
-                if( dp[i-1][j] == dp[i-1][j+1]):
-                    c += 1;
-                else:
-                    dp[i] += chr(c+1+ord('0')) + dp[i-1][j] ;
-                    c=0;
-            dp[i] += ' ';
-            i +=1
-            
-
-        return dp[-1][:-1];
-"""class Solution:
     def count(self, s):
-        si, ei, result = 0, 0, ""
-        while ei+1 < len(s):
-            # print(f"({si}, {ei}) {s[si:ei+1]}")
-            if s[ei] != s[ei+1]:
-                result += f"{len(s[si:ei+1])}{s[si]}"
+        si, ei, result = 0, 0, []
+        while ei + 1 < len(s):
+            if s[ei] != s[ei + 1]:
+                result.append(str(ei - si + 1))
+                result.append(s[si])
                 si = ei = ei + 1
             else:
                 ei += 1
-        result += f"{len(s[si:ei+1])}{s[si]}"
-        return result
-    
+        result.append(str(ei - si + 1))
+        result.append(s[si])
+        return "".join(result)
+
     def countAndSay(self, n: int) -> str:
         i, result = 3, "11"
         if n == 1:
             return "1"
         elif n == 2:
             return "11"
-        
+
         while i <= n:
             result = self.count(result)
-            # print(f"[{i}] {result}")
             i += 1
-        return result"""
+        return result

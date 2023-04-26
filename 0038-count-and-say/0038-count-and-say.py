0b004809a144bc33d05f755ae7d96d8b22fa2478
JAESEO LEE <goodstart57@gmail.com>
2023-04-26 21:24:57 +0900

    Time: 51 ms (37.03%), Space: 14 MB (42.00%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.py
+++ 0038-count-and-say/0038-count-and-say.py
@@ -1,4 +1,23 @@
 class Solution:
+    def countAndSay(self, n: int) -> str:
+        if n==1:
+            return "1"
+        x=self.countAndSay(n-1)
+        s=""
+        y=x[0]
+        ct=1
+        for i in range(1,len(x)):
+            if x[i]==y:
+                ct+=1
+            else:
+                s+=str(ct)
+                s+=str(y)
+                y=x[i]
+                ct=1
+        s+=str(ct)
+        s+=str(y)
+        return s
+"""class Solution:
     def count(self, s):
         si, ei, result = 0, 0, ""
         while ei+1 < len(s):
@@ -22,8 +41,4 @@ class Solution:
             result = self.count(result)
             # print(f"[{i}] {result}")
             i += 1
-        return result
-        
-        
-        
-        
+        return result"""

142d933b820f5a9f075f54a71f1c137b4b2bd29e
JAESEO LEE <goodstart57@gmail.com>
2023-05-01 17:28:35 +0900

    Time: 48 ms (5.49%), Space: 16.2 MB (8.21%) - LeetHub

--- 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.py
+++ 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.py
@@ -1,21 +1,4 @@
 class Solution:
     def average(self, salary: List[int]) -> float:
-        salary.sort()
-        return (sum(salary) - salary[0] - salary[-1]) / (len(salary) - 2)
-    
-    """def average(self, salary: List[int]) -> float:
-        return (sum(salary) - max(salary) - min(salary)) / (len(salary) - 2)"""
-    
-    """def average(self, salary: List[int]) -> float:
-        salary.sort()
-        return sum(salary[1:-1]) / len(salary[1:-1])"""
-    
-    """def average(self, salary: List[int]) -> float:
-        cnt, salary_sum, salary_max, salary_min = 0, 0, 0, 10**6+1
-        for s in salary:
-            cnt += 1
-            salary_sum += s
-            salary_max = max(s, salary_max)
-            salary_min = min(s, salary_min)
-        
-        return (salary_sum - salary_max - salary_min) / (cnt - 2)"""
+        salary = sorted(salary)[1:len(salary)-1]
+        return sum(salary) / len(salary)

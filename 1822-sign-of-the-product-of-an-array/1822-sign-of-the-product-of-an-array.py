0b8e069d47cb79522d49974a6c3623cbbcc86ef1
JAESEO LEE <goodstart57@gmail.com>
2023-05-02 20:37:56 +0900

    Time: 79 ms (5.64%), Space: 16.6 MB (7.53%) - LeetHub

--- 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.py
+++ 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.py
@@ -1,8 +1,23 @@
 class Solution:
     def arraySign(self, nums: List[int]) -> int:
+        nums = sorted(nums)
+        l, r = 0, len(nums)
+        while l < r:
+            m = (l + r) // 2
+            # print(f"{l} / {m} / {r}")
+            if nums[m] == 0:
+                return 0
+            elif nums[m] < 0:
+                l = m + 1
+            else:
+                r = m
+        return 1 if l % 2 == 0 else -1
+        
+        
+    """def arraySign(self, nums: List[int]) -> int:
         result = 1
         for num in nums:
             if num == 0:
                 return num
             result *= num
-        return 1 if result > 0 else -1
+        return 1 if result > 0 else -1"""
